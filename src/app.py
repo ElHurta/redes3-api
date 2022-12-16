dea27eaee02a1310edcebc1c2327608af0c23662
ElHurta <juan.david.hurtado08@gmail.com>
2022-12-16 01:35:55 -0500

    CORS

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.pop_routes import pop_routes
 from routes.smtp_routes import smtp_routes
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('config')
     app.register_blueprint(pop_routes)
     app.register_blueprint(smtp_routes)
